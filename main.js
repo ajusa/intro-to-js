87df47478aaa20b2179b13bad4743c18e4281d84
Arham Jain <jaina18@okemosschools.net>
2018-02-08 10:32:36 -0500

    init, added basics

--- main.js
+++ main.js
@@ -4,18 +4,19 @@
 for multiline. Similar to Java, C++, C, and many other languages. Different from Python
 */
 
-/*
+
 // VARIABLES //
 var a = 5 //creates a variable "a", with the number value 5
 b = 7 //you don't need the var keyword to declare a new variable, similar to Python.
 myString = "Hello world!" //declare strings like so.
 arr = [] //declares an empty array
 arr2 = [1, 2, 3, 4] //creates an array with a few numbers as elements.
-*/
-/*
+fun = true //simple booleans
+
+
 // HELLO WORLD //
 console.log(myString) //prints out "Hello world!". console.log takes in pretty much any arguements and prints it out.
-*/
+
 
 // CONTROL FLOW //
 if(a == 5){ //simple if statement
@@ -23,15 +24,75 @@ if(a == 5){ //simple if statement
 } else if(b==5){ //simple else if
 	//will not be run
 }
-for (var i = arr2.length - 1; i >= 0; i--) { //this is a C style for loop, iterates through every element
-	arr2[i]
+
+for(var i = 0; i < arr2.length; i++) { //this is a C style for loop, iterates through every element
+	console.log(arr2[i])
+}
+
+for(var i = arr2.length - 1; i >= 0; i--) { //this is a C style for loop, iterates through every element
+	console.log(arr2[i])
 }
 //It starts at the end of the array, however. If you were to print this out, it would go "4 3 2 1".
 
-for (var i = 0; i < arr.length; i++) { //more standard for loop, iterates through in order
-	arr[i]
+for(item of arr2) { //this iterates over each item in an array, similar to Python
+  console.log(item)
 }
 
-for each (item in arr2) {
-  console.log(item)
+i = 0
+while(i < arr2.length){ //you can always use a while loop to do the same thing
+	console.log(arr2[i])
+	i++
+}
+
+// FUNCTIONS INTRO //
+//why is this intro? JS functions are whack. They do everything and it sometimes will trigger people
+//let's create a simple add function
+function add(a, b){
+	return //your code here
+}
+
+console.log(add(5,4)) //should print out 9
+
+//Javascript also supports passing in functions as arguments. It is confusing, but can be useful
+times = function(func, x){ //we want this function to execute "func" x number of times
+	//try doing this with a while loop!
+}
+//notice how we assigned a variable to be a function. 
+//JS has functions be actual types, so assigning it does the same thing as above with "add"
+
+times(function(){console.log("this is confusing")}, 3) //should print out that string 3 times.
+
+// JavaScript Objects //
+//most of you have probably seen or used a .json file.
+//JSON stands for Java Script Object Notation. These are quite similar to Python dictionaries.
+obj = {
+	key: "value",
+	age: 21,
+	"canDrink": true, //why does this have quotes?
+	"is lit": false
+}
+//you use the "." to access properties of an object
+console.log(obj.key) //prints out value
+console.log(obj.age) //prints out 21
+console.log(obj["key"]) //also prints out value. Hmmm
+console.log(obj.canDrink) //prints out true
+//console.log(obj.is lit) //WON'T WORK
+console.log(obj["is lit"]) //prints out false
+//we can use a string as a "key" to have special chars in our keys, like spaces.
+obj["newKey"] = 5 //creates a new key and assigns a value
+obj.newKey = 6
+console.log(obj.newKey) //what will this print out?
+
+// Object Orientation //
+//oh boy, classes. One of the most confusing things about JS.
+//let's make a todo class
+function todo(name){
+	this.name = name
+	this.isDone = false
+	this.checkOff = function(){
+		this.isDone = true
+	}
+	this.print = function(){
+		console.log(this)
+	}
 }
